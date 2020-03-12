07913b79a3736288d9c90953e26a9949ac85ad85
amit <techamit10.imcve@gmail.com>
2020-03-12 13:15:07 +0530

    This is test commit

---
+++ Get_repo_sss/Get_repo_sss/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Get_repo_sss.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
